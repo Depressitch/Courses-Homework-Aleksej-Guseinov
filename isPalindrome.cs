6f246c9d680d85671ae808496729579c20c7917f
Depressitch <42008237+Depressitch@users.noreply.github.com>
2020-03-27 18:50:55 +0300

    Update isPalindrome.cs

--- isPalindrome.cs
+++ isPalindrome.cs
@@ -1,5 +1,5 @@
 using System;
-					
+
 public class Program
 {
 	public static void Main()
@@ -11,27 +11,18 @@ public class Program
 	}
 	static void IsPalindrome(string text) //функция на проверку является ли строка палиндромом
 	{
-		string modText = text; 	//создаём другую строку, в которой не будет пробелов
-		DltSpaces(modText); 	//удаляем из строки пробелы
+		string modText = text;  //создаём другую строку, в которой не будет пробелов
+		modText = modText.ToLower(); //делаем все буквы нижнего регистра
+		modText = modText.Replace(" ", ""); //удаляем пробелы
 		for (int i = 0; i < modText.Length / 2; i++)
 		{
 			//если находится не "зеркальный" элемент, то сразу выводится текст о том, что это не палиндром + заканчивается функция
-			if(modText[i] != modText[text.Length - i - 1]) 
+			if (modText[i] != modText[modText.Length - i - 1])
 			{
 				Console.WriteLine("This line is not a palindrome.");
 				return;
 			}
 		}
 		Console.WriteLine("This line is a palindrome.");
-	}
-	static void DltSpaces(string text) 	//функция удаляет пробелы из строки
-	{
-		for (int i = 0; i < text.Length; i++)
-		{
-			if (text[i] == ' ')
-			{
-				text.Remove(i, 1);				
-			}
-		}
 	}
 }
