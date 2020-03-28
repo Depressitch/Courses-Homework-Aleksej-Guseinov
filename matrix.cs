35ddabebbd50610eb13982b9ea398a1d0dc33c13
Depressitch <42008237+Depressitch@users.noreply.github.com>
2020-03-28 17:59:44 +0300

    Update matrix.cs

--- matrix.cs
+++ matrix.cs
@@ -6,14 +6,22 @@ public class Program
 	{
 		Console.Write("Input sizes of first matrix (a - height, b - width):\na: ");
 		int a = Convert.ToInt32(Console.ReadLine());
-		Console.Write("b: ");
+		Console.WriteLine("b: ");
 		int b = Convert.ToInt32(Console.ReadLine());
-		int[,] mtrOne = inputMatrix (a, b);
+		int [,] mtrOne = genRandMatrix (a, b);
+		Console.WriteLine("====================\n" + "Matrix one: ");
+		outputMatrix(mtrOne);
+		//int[,] mtrOne = inputMatrix (a, b);
 		Console.Write("Input sizes of second matrix (a - height, b - width):\na: ");
 		a = Convert.ToInt32(Console.ReadLine());
-		Console.Write("b: ");
+		Console.WriteLine("b: ");
 		b = Convert.ToInt32(Console.ReadLine());
-		int[,] mtTwo = inputMatrix (a, b);
+		int [,] mtrTwo = genRandMatrix (a, b);
+		Console.WriteLine("====================\n" + "Matrix two: ");
+		outputMatrix(mtrTwo);
+		Console.WriteLine("====================\n" + "Result of multiplied matrixes: ");
+		outputMatrix(multMatrix(mtrOne, mtrTwo));
+		//int[,] mtTwo = inputMatrix (a, b);
 	}
 	static int[,] inputMatrix (int a, int b)
 	{
@@ -22,6 +30,17 @@ public class Program
 			for (int j = 0; j < b; j++)
 			{
 				newMatrix [i, j] = Convert.ToInt32(Console.ReadLine());
+			}
+		return newMatrix;
+	}
+	static int[,] genRandMatrix (int a, int b)
+	{
+		int [,] newMatrix = new int[a, b];
+		Random random = new Random();  
+		for (int i = 0; i < a; i++)
+			for (int j = 0; j < b; j++)
+			{
+				newMatrix [i, j] = random.Next(0, 9);;
 			}
 		return newMatrix;
 	}
@@ -38,7 +57,14 @@ public class Program
 	}
 	static int[,] multMatrix (int [,] matrixOne, int[,] matrixTwo)
 	{
-		int[,] newMatrix = new int []; 
+		int[,] newMatrix = new int [matrixOne.GetLength(0), matrixTwo.GetLength(1)]; 
+		for (int i = 0; i < newMatrix.GetLength(0); i++)
+		{
+			for (int j = 0; j < newMatrix.GetLength(1); i++)
+			{
+				newMatrix[i,j] = ;
+			}
+		}
 		return newMatrix;
 	}
 }
