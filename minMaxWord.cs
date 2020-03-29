b0326f1e9a2b2556d5f54a5e13a550d5cc4af11b
Depressitch <42008237+Depressitch@users.noreply.github.com>
2020-03-29 14:34:38 +0300

    Update minMaxWord.cs

--- minMaxWord.cs
+++ minMaxWord.cs
@@ -4,22 +4,21 @@ public class Program
 {
 	public static void Main()
 	{
-		Console.WriteLine("Input your line: ");
+		Console.Write("Input your line: ");
 		string input = Console.ReadLine();
 		FindMinMaxWord(input);
 		Console.ReadLine();
 	}
 	static void FindMinMaxWord(string text) //функция на нахождение самого длинного и короткого слова
-	{
-		var modText = text.ToLower();
-		int minLength = modText.Length;
+	{  
+		int minLength = text.Length;
 		int minI = 0;
 		int maxLength = 0;
 		int maxI = 0;
-		for (int i = 0; i < modText.Length; i++)
+		for (int i = 0; i < text.Length; i++)
 		{
 			int wordLength = 0;
-			for (int j = i; j < modText.Length && char.IsLetter(modText, j); j++) //если символ - буква, то увеличивается длина слова
+			for (int j = i; j < text.Length && char.IsLetter((string)text, j); j++) //если символ - буква, то увеличивается длина слова
 			{
 				wordLength++;
 			}
@@ -35,6 +34,7 @@ public class Program
 			}
 			i += wordLength;
 		}
-		Console.WriteLine($"Longest word: {modText.Substring(maxI, maxLength)} \nShortest word: {modText.Substring(minI, minLength)}"); 
+		Console.WriteLine($"Longest word: {text.Substring(maxI, maxLength)} \nShortest word: {text.Substring(minI, minLength)}"); 
+		//выводим мин/макс слова подстроками
 	}
 }
